8dce9c705226d35e5599c76a8c1c5557f95116a3
Mohamed Ramadan <mohamed_ramadan154@hotmail.com>
2018-07-13 17:21:49 +0200

    Unfavorite functionality

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -48,4 +48,12 @@ export class QuotesPage implements OnInit {
     alert.present();
   }
 
+  onRemoveFromFavorites(quote: Quote) {
+    this.quotesService.removeQuoteFromFavorites(quote);
+  }
+
+  isFavorite(quote: Quote) {
+    return this.quotesService.isQuoteFavorite(quote);
+  }
+
 }
